d1ec9ed81a582e79a8cca59c6020cb8259ebb963
jesg <savagejas@gmail.com>
2013-11-07 05:14:20 -0600

    section 3.8.2 fifo queue solution

---
+++ src/main/java/concurrency/FifoQueue.java
@@ -0,0 +1,29 @@
+package concurrency;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.Semaphore;
+
+public class FifoQueue {
+	private final Semaphore mutex = new Semaphore(1);
+	private final Queue<Semaphore> queue = new LinkedList<Semaphore>();
+
+	public void await(){
+		Semaphore localSemaphore = new Semaphore(0);
+		
+		mutex.acquireUninterruptibly();
+		queue.add(localSemaphore);
+		mutex.release();
+		
+		localSemaphore.acquireUninterruptibly();
+	}
+	
+	public void signal(){
+		Semaphore localSemaphore;
+		mutex.acquireUninterruptibly();
+		localSemaphore = queue.poll();
+		mutex.release();
+		
+		localSemaphore.release();
+	}
+}
